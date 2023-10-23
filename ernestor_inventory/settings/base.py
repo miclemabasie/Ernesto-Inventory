aae3e5cb2d1152c2748ab80a758443fe7cfc5486
Miclem <miclemabasie3@gmail.com>
2023-10-24 00:28:43 +0100

    modified settings for deploy

--- ernestor_inventory/settings/base.py
+++ ernestor_inventory/settings/base.py
@@ -19,10 +19,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "dkjal;ksdfjlaksdjflkasjdfklajsdklfjaslk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", "now.sh", "localhost", "127.0.0.1"]
 
--- ernestor_inventory/settings/development.py
+++ ernestor_inventory/settings/development.py
@@ -12,14 +12,25 @@ from .base import *
 # }
 
 #
+# DATABASES = {
+#     "default": {
+#         "ENGINE": env("DATABASE"),
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PASSWORD"),
+#         "HOST": env("DB_HOST"),
+#         "PORT": env("DB_PORT"),
+#     }
+# }
+#
+
 DATABASES = {
     "default": {
-        "ENGINE": env("DATABASE"),
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "eqs7n7ucX5SHxFXiu1CX",
+        "HOST": "containers-us-west-102.railway.app",
+        "PORT": "7463",
     }
 }
-#
