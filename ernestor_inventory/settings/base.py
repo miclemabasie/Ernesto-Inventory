591974b9d33d2c62b5c6c6bc2d92d433465a4ea6
Miclem <miclemabasie3@gmail.com>
2023-10-24 01:00:35 +0100

    modified settings for deploy

--- ernestor_inventory/settings/base.py
+++ ernestor_inventory/settings/base.py
@@ -119,6 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# vercel_app/settings.py
+WSGI_APPLICATION = "vercel_app.wsgi.app"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
